74a27929595ee6d12da71fc82c0e1c68a2ec46d5
5amstag <73161088+5amstag@users.noreply.github.com>
2020-10-20 10:00:13 +0200

    Update edraw.py

--- edraw.py
+++ edraw.py
@@ -5,9 +5,11 @@
 VERSION
 -------
 version 2 of edraw inkscape extension
+
 AUTHOR
 ------
 Lukas Geiling, 2020
+
 LICENSE
 -------
 This program is free software; you can redistribute it and/or modify
@@ -23,6 +25,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 TODO
 ------
 * testing
